32b391e99eae82e7f377ec67111996bdf6ea9ca3
mehrKind <107022119+mehrKind@users.noreply.github.com>
2022-08-02 11:06:22 +0430

    Delete djangoBlog directory

--- djangoBlog/articles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "articles"
-urlpatterns = [
-    path('', views.articles_list,name="list"),
-    path('create',views.create_views, name="create"),
-    path('<slug>',views.articles_details,name="detail"),
-]
